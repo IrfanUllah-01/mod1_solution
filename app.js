f681b5be2ee0cce3bb0dc330e4098c5c5439e11a
IrfanUllah-01 <54708821+IrfanUllah-01@users.noreply.github.com>
2019-08-30 20:43:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+(function () {
+    'use strict';
+
+    angular.module('LunchCheck', [])
+
+        .controller('LunchCheckController', function ($scope) {
+            $scope.value = "first";
+            $scope.Message = "";
+            $scope.CheckValue = function () {
+                if ($scope.value.length == 0) {
+                    $scope.Message = "Enter data.";
+                }
+                else {
+                    var items = $scope.value.split(',');
+                    if (items.length <= 3)
+                        $scope.Message = "Enjoy";
+                    else 
+                        $scope.Message = "Too Much";
+                }
+            };
+        });
+
+})();
